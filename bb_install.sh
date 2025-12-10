18f9f6ae0bc71f5d0c2c6507167d2dc16f8d092e
c0untach <51246376+c0untach@users.noreply.github.com>
2025-12-10 22:33:28 +0200

    Create bb_install.sh

---
+++ bb_install.sh
@@ -0,0 +1,14 @@
+# update repos
+apt update -y
+
+# install packages from kali repos
+apt install jq uro golang -y
+
+# install gau and check version
+git clone https://github.com/lc/gau.git
+cd gau/cmd/gau
+go build
+mv gau /usr/local/bin/
+echo -e "+++[!] gau installed with version: [!]+++"
+gau --version
+echo -e ""
